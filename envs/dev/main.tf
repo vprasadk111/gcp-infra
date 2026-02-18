cae911a73d1fa8a9572d2a9fdae01662d69401d8
vprasadk111 <84302211+vprasadk111@users.noreply.github.com>
2026-02-18 18:44:53 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("/Users/vishnuprasadk/Personal/Learning/Terraform/Terraform_2024_Latest/GCP/TERRAFORM_2026/MAIN PRO/weighty-utility-472804-d2-5fb6ab8dac46.json")
+  #credentials = file("/Users/vishnuprasadk/Personal/Learning/Terraform/Terraform_2024_Latest/GCP/TERRAFORM_2026/MAIN PRO/weighty-utility-472804-d2-5fb6ab8dac46.json")
   project = var.project_id
   region  = var.region
 }
